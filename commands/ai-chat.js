050ea638689746e5560f8fad3fd30f8d94d62310
Taimoor <77613021+TaimHu@users.noreply.github.com>
2022-09-13 11:57:03 +0300

    Update ai-chat.js

--- commands/ai-chat.js
+++ commands/ai-chat.js
@@ -3,26 +3,33 @@ module.exports = {
     name: 'ai-chat',
     aliases: [],
     description: 'this is ai-chat command',
-    execute(client, receivedMessage,command, args, Discord) {
+    async execute(client, receivedMessage,command, args, Discord) {
         const channel = receivedMessage.guild.channels.cache.find(c => c.name === 'ai-chat');
         if(!channel) return
         const usertext = receivedMessage.content.toString()
+        console.log(usertext)
         
             const MONKEY_API = 'SE2SoHPqfQ0mkJ9UMf9uXimmK'
            
-        fetch(`http://api.brainshop.ai/get?bid=169199&key=5riVVxrFoizwxSiI&uid=${receivedMessage.author.id}&msg=${usertext}`)
-        
-
-        .then(response => response.json())
-        .then(data => {
-            setTimeout(() => {
-            channel.send(data.response)
-        }, 5000)
+         const  response = await fetch(`http://api.brainshop.ai/get?bid=169199&key=5riVVxrFoizwxSiI&uid=${receivedMessage.author.id}&msg=${usertext}`)
+        // console.log(response)
+         const data  = await response.json()
+         const reply = data.cnt;
+        console.log(reply);
+        channel.send(reply)
+        // .then(response => {
+        //     console.log(response)
+        //     response.json()
+        // })
+        // .then(data => {
+        //     setTimeout(() => {
+        //     channel.send(data.response)
+        // }, 5000)
             
-        })
-        .catch(()=>{
-            channel.send("I'm Sorry, I didn't quite get that. Let me rest for a bit, I'll reply later Zzz")
-        })
+        // })
+        // .catch(()=>{
+        //     channel.send("I'm Sorry, I didn't quite get that. Let me rest for a bit, I'll reply later Zzz")
+        // })
 
 
     }
